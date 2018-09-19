cbe4a26a5acfe384403d8f35db73361c626f7d73
ak32pvi <vpalamarchuck@gmail.com>
2018-09-19 15:16:03 +0300

    v2.1

--- test/test_create_project.py
+++ test/test_create_project.py
@@ -1,6 +1,10 @@
+from model.project import Project
+
+
 def test_create_project(app):
     app.session.login("administrator", "administrator")
     old_projects = app.project.get_project_list()
+    project = Project(name="name")
     app.project.create()
     new_projects = app.project.get_project_list()
     assert len(old_projects) + 1 == len(new_projects)
